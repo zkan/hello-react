151ded28f23499cac7f7eaedd397c1611ac8024e
zkan <kan@prontomarketing.com>
2017-03-15 13:26:55 +0700

    Create Hello component taking props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Hello = ({ firstName, lastName }) => {
+  return (<h1>Hello, { firstName } { lastName }</h1>)
+}
+
 ReactDOM.render(
-  <h1>Hello, React!</h1>,
+  <Hello
+    firstName="Kan"
+    lastName="Ouivirach"
+  />,
   document.getElementById('react-root')
 )
