3ac9a3e50adf112fa0eb750e72c8a02285db68d4
zkan <kan@prontomarketing.com>
2017-03-15 13:43:58 +0700

    Create counter component with state and render

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
+import Counter from './components/Counter.js'
 
 const Hello = ({ firstName, lastName }) => {
   return (<h1>Hello, { firstName } { lastName }</h1>)
 }
 
 ReactDOM.render(
-  <Hello
-    firstName="Kan"
-    lastName="Ouivirach"
-  />,
+  <Counter label="I'm a counter" />,
   document.getElementById('react-root')
 )
