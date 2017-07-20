5049ad15aaad47968a7c229659ed7045d9d147e2
zkan <kan@prontomarketing.com>
2017-07-20 13:37:05 +0700

    Initialize by create-react-app :bear:

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import './style.css'
-import Counter from './components/Counter'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const Hello = ({ firstName, lastName }) => {
-  return (<h1>Hello, { firstName } { lastName }</h1>)
-}
-
-const render = (Counter) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Counter label="Hello, I'm a counter." />
-    </AppContainer>,
-    document.getElementById('react-root')
-  )
-}
-
-render(Counter)
-
-if (module.hot) {
-  module.hot.accept('./components/Counter', () => {
-    render(Counter)
-  })
-}
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
