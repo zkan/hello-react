8822fd751c38b3cbe7de56bfc3484324b456bc99
zkan <kan@prontomarketing.com>
2017-07-27 20:43:15 +0700

    Enable Redux DevTools
    GitHub: https://github.com/zalmoxisus/redux-devtools-extension

--- 08-redux/src/index.js
+++ 08-redux/src/index.js
@@ -6,7 +6,10 @@ import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
