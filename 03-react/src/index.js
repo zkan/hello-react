aeec749787f6e0063505faccf2bff75e5c5912e5
zkan <kan@prontomarketing.com>
2017-07-27 14:45:31 +0700

    Use babel-plugin-transform-class-properties

--- 03-react/src/index.js
+++ 03-react/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Button extends React.Component {
+  state = {
+    count: 0
+  }
+
+  increaseCount = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.increaseCount}>{this.state.count}</button>
+    )
+  }
+}
+
 const App = () => (
-  <h1>Hello, React!</h1>
+  <div>
+    <h1>Hello, React!</h1>
+    <Button />
+  </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
