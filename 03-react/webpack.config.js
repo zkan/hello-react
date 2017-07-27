947966dc9c5a83971094aea85ebec9013fab3bf1
zkan <kan@prontomarketing.com>
2017-07-27 14:41:24 +0700

    Set up React project with Webpack

---
+++ 03-react/webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path')
+
+module.exports = {
+  entry: path.resolve('src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+      },
+    ],
+  }
+}
