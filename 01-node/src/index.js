1059b0c77945f7f6d756962891cf5d9724712300
zkan <kan@prontomarketing.com>
2017-07-27 14:02:34 +0700

    Use square from import

--- 01-node/src/index.js
+++ 01-node/src/index.js
@@ -13,3 +13,5 @@ const numbers = [1, 2, 3]
 console.log(numbers.map(number => number * 2))
 console.log(numbers.filter(number => number % 2 == 0))
 console.log(numbers.reduce((total, value) => total + value))
+
+console.log(square(5))
